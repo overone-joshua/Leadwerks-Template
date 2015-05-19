c1c7ac6d00b332368129fb5aa2571235af8737b7
Joshua Allen <joshua@edensoftworks.net>
2015-05-19 13:03:28 -0400

    Implemented boilerplate Leadwerks Application.

--- Source/App.cpp
+++ Source/App.cpp
@@ -1,13 +1,145 @@
+#pragma once
 #include "App.h"
+#include "Leadwerks.h"
 
-using namespace Leadwerks;
+#include <iostream>
 
-App::App() : window(NULL), context(NULL), world(NULL), camera(NULL) {}
+/* Externals From App.h */
+App* gApp;
 
-App::~App() { delete world; delete window; }
+// -----
 
-Vec3 camerarotation;
-bool freelookmode=true;
+std::string App::Name() {
+	return this->m_name;
+}
+
+unsigned App::ScreenWidth() {
+	return this->m_nScreenWidth;
+}
+
+unsigned App::ScreenHeight() {
+	return m_nScreenHeight;
+}
+
+bool App::Fullscreen() {
+	return this->m_bFullscreen;
+}
+
+bool App::Paused() {
+	return this->m_bPaused;
+}
+
+void App::setName(std::string name) {
+	this->m_name = name;
+}
+
+void App::setScreenWidth(unsigned width) {
+	this->m_nScreenWidth = width;
+}
+
+void App::setScreenHeight(unsigned height) {
+	this->m_nScreenHeight = height;
+}
+
+void App::ToggleFullscreen() {
+	this->m_bFullscreen = !this->m_bFullscreen;
+}
+
+void App::Pause() {
+	this->m_bPaused = true;
+}
+
+void App::UnPause() {
+	this->m_bPaused = false;
+}
+
+Leadwerks::Window& App::GetWindow() {
+	return *this->m_pWindow;
+}
+
+Leadwerks::Context& App::GetContext() {
+	return *this->m_pContext;
+}
+
+Leadwerks::World& App::GetWorld() {
+	return *this->m_pWorld;
+}
+
+Leadwerks::Camera& App::GetCamera() {
+	return *this->m_pCamera;
+}
+
+int App::CreateWindow(const int flags) {
+	if (this->m_pWindow != nullptr) { this->m_pWindow->Release(); }
+	this->m_pWindow = Leadwerks::Window::Create(this->m_name, 0, 0, this->m_nScreenWidth, this->m_nScreenHeight, flags);
+	if (this->m_pWindow == nullptr) { std::cout << "Window creation was unsuccessful. \n"; return WINDOW_CREATION_ERROR; }
+	return 0;
+}
+
+int App::CreateContext(const int flags) {
+	if (this->m_pContext != nullptr) { this->m_pContext->Release(); }
+	this->m_pContext = Leadwerks::Context::Create(this->m_pWindow, flags);
+	if (this->m_pContext == nullptr) { std::cout << "Context creation was unsuccessful. \n"; return CONTEXT_CREATION_ERROR; }
+	return 0;
+}
+
+int App::CreateWorld() {
+	if (this->m_pWorld != nullptr) { this->m_pWorld->Release(); }
+	this->m_pWorld = Leadwerks::World::Create();
+	if (this->m_pWorld == nullptr) { std::cout << "World creation was unsuccessful. \n"; return WORLD_CREATION_ERROR; }
+	return 0;
+}
+
+int App::CreateCamera(Leadwerks::Entity* pParent) {
+	if (this->m_pCamera != nullptr) { this->m_pCamera->Release(); }
+	this->m_pCamera = Leadwerks::Camera::Create(pParent);
+	if (this->m_pCamera == nullptr) { std::cout << "Camera creation was unsiccessful. \n"; return CAMERA_CREATION_ERROR; }
+	return 0;
+}
+
+int App::Initialize() {
+	int initResults = 0;
+	/* Window */
+	initResults |= (this->m_bFullscreen) ? this->CreateWindow(Leadwerks::Window::FullScreen) : this->CreateWindow(Leadwerks::Window::Titlebar | Leadwerks::Window::Center);
+
+	/* Context */
+	initResults |= this->CreateContext();
+
+	/* World */
+	initResults |= this->CreateWorld();
+
+	/* Camera */
+	initResults |= this->CreateCamera(nullptr);
+
+	this->m_bSettingsChangedThisFrame = false;
+	return initResults;
+}
+
+void App::Shutdown() {
+	/* Perform any component-shutdown logic */
+
+}
+
+App::App()
+	: m_pWindow(nullptr), m_pContext(nullptr), m_pWorld(nullptr), m_pCamera(nullptr) {
+
+	gApp = this;
+
+	this->m_nScreenWidth = 1024.0f;
+	this->m_nScreenHeight = 768.0f;
+	
+	this->m_bFullscreen = false;
+	this->m_bPaused = false;
+	this->m_bSettingsChangedThisFrame = true;
+	this->m_bStateChangedThisFrame = true;
+}
+
+App::~App() { 
+	this->Shutdown();
+
+	SAFE_DELETE(this->m_pWorld); 
+	SAFE_DELETE(this->m_pWindow);
+}
 
 bool App::Start()
 {
@@ -18,71 +150,54 @@ bool App::Start()
 		return false;
 	}*/
 
-	//Create a window
-	window = Leadwerks::Window::Create("ViewTheHorizon");
-	
-	//Create a context
-	context = Context::Create(window);
-	
-	//Create a world
-	world = World::Create();
-	
-	//Create a camera
-	camera = Camera::Create();
-	camera->Move(0,2,-5);
-	
-	//Hide the mouse cursor
-	window->HideMouse();
-	
-	std::string mapname = System::GetProperty("map","Maps/start.map");
-	Map::Load(mapname);
-	
-	//Move the mouse to the center of the screen
-	window->SetMousePosition(context->GetWidth()/2,context->GetHeight()/2);
-	
+	/* Initialize the application */
+	if (this->Initialize() != 0) { return false; }
+
+	/* Center and, or Hide Mouse */
+	//this->m_pWindow->HideMouse();
+	float mX = this->m_pWindow->GetWidth() * 0.5f; float mY = this->m_pWindow->GetHeight() * 0.5f;
+	this->m_pWindow->SetMousePosition(mX, mY);
+
+	/* Populate the new camera with default values */
+	Leadwerks::Vec3 vZero = Leadwerks::Vec3(0.0f, 0.0f, 0.0f);
+	this->m_pCamera->SetRotation(vZero, false);
+	this->m_pCamera->SetRotation(vZero, true);
+	this->m_pCamera->SetPosition(vZero, false);
+	this->m_pCamera->SetPosition(vZero, true);
+
+	/* Move camera back 5.0f meters */
+	this->m_pCamera->Move(0.0f, 0.0f, -5.0f, true);
+
 	return true;
 }
 
 bool App::Loop()
 {
-	//Close the window to end the program
-	if (window->Closed()) return false;
-    
-	//Press escape to end freelook mode
-	if (window->KeyHit(Key::Escape))
-	{
-		if (!freelookmode) return false;
-		freelookmode=false;
-		window->ShowMouse();
-	}
-	
-	if (freelookmode)
-	{
-		//Keyboard movement
-		float strafe = (window->KeyDown(Key::D) - window->KeyDown(Key::A))*Leadwerks::Time::GetSpeed() * 0.05;
-		float move = (window->KeyDown(Key::W) - window->KeyDown(Key::S))*Leadwerks::Time::GetSpeed() * 0.05;
-		camera->Move(strafe,0,move);
+	/* If the window is closed, for any reason, end the application */
+	if (this->m_pWindow->Closed()) { return false; }
 
-		//Get the mouse movement
-		float sx = context->GetWidth()/2;
-		float sy = context->GetHeight()/2;
-		Vec3 mouseposition = window->GetMousePosition();
-		float dx = mouseposition.x - sx;
-		float dy = mouseposition.y - sy;
+	/* Update the application's game-time, at 60fps */
+	Leadwerks::Time::Update(60);
 
-		//Adjust and set the camera rotation
-		camerarotation.x += dy / 10.0;
-		camerarotation.y += dx / 10.0;
-		camera->SetRotation(camerarotation);
-
-		//Move the mouse to the center of the screen
-		window->SetMousePosition(sx,sy);
+	/* If the application's display settings changes this frame, re-initialize the application */
+	if (this->m_bSettingsChangedThisFrame) {
+		this->Initialize();
+		return true;
 	}
 
-	Leadwerks::Time::Update();
-	world->Update();
-	world->Render();
-	context->Sync(false);
-	
+	if (!this->m_bPaused) {
+		/* Perform game-loop updates here */
+		if (this->m_pWorld != nullptr) { this->m_pWorld->Update(); }
+		
+	}
+
+	/* Perform any 3D-rendering here */
+	if (this->m_pWorld != nullptr) { this->m_pWorld->Render(); }
+
+	this->m_pContext->SetBlendMode(Leadwerks::Blend::Alpha);
+	/* Perform any 2D-rendering here.*/
+	this->m_pContext->SetBlendMode(Leadwerks::Blend::Solid);
+
+	this->m_pContext->Sync(false);
 	return true;
 }
--- Source/App.h
+++ Source/App.h
@@ -1,19 +1,81 @@
+#ifndef _APP_H_
+	#define _APP_H_
+
 #pragma once
 #include "Leadwerks.h"
 
-using namespace Leadwerks;
+// MACROS
+#define SAFE_DELETE( p )       { if( p ) { delete ( p );     ( p ) = NULL; } }
+#define SAFE_DELETE_ARRAY( p ) { if( p ) { delete[] ( p );   ( p ) = NULL; } }
+#define SAFE_RELEASE( p )      { if( p ) { ( p )->Release(); ( p ) = NULL; } }
+
+// -----
+
+// CUSTOM ERROR Definitions
+#define WINDOW_CREATION_ERROR	2
+#define CONTEXT_CREATION_ERROR	4
+#define WORLD_CREATION_ERROR	8
+#define CAMERA_CREATION_ERROR	16
+
+// -----
 
 class App
 {
 public:
-	Leadwerks::Window* window;
-	Context* context;
-	World* world;
-	Camera* camera;
-
-	App();
-	virtual ~App();
+										App();													// Application constructor.
+	virtual								~App();													// Application destructor.
 	
-    virtual bool Start();
-    virtual bool Loop();
-};
+	virtual bool						Start();												// Called to bootstrap the application.
+	virtual bool						Loop();													// The main game loop that runs every frame.
+
+	Leadwerks::Window&					GetWindow();											// Gets the window handle.
+	Leadwerks::Context&					GetContext();											// Gets the context handle.
+	Leadwerks::World&					GetWorld();												// Gets the world handle.
+	Leadwerks::Camera&					GetCamera();											// Gets the camera handle.
+
+	std::string							Name();													// Gets the name of the application.
+	unsigned							ScreenWidth();											// Gets the current width of the application screen.
+	unsigned							ScreenHeight();											// Gets the current height of the application screen.
+	bool								Fullscreen();											// Indicates whether the application is currently in full-screen.
+	bool								Paused();												// Indicates whether the application is currently paused.
+
+	void								setName(std::string name);								// Sets the name of the application.
+	void								setScreenWidth(unsigned width);							// Sets the width if the application screen.
+	void								setScreenHeight(unsigned height);						// Sets the height of the application screen.
+
+	void								ToggleFullscreen();										// Toggles full-screen mode for the application.
+	void								Pause();												// Pauses the application.
+	void								UnPause();												// Un-pauses the application.
+	
+protected:
+	int									Initialize();											// Initializes the application.
+	void								Shutdown();												// Shuts down the application.
+	
+	int									CreateWindow(const int flags = 0);						// Creates the application's window, using the given flags.
+	int									CreateContext(const int flags = 0);						// Creates the application's rendering context, using the given flags.
+	int									CreateWorld();											// Creates the application's primary world.
+	int									CreateCamera(Leadwerks::Entity* pParent = nullptr);		// Create the application's primary camera, using the given parent.
+
+private:
+	Leadwerks::Window*					m_pWindow;												// The main window handle for the application.
+	Leadwerks::Context*					m_pContext;												// The main rendering context handle for the application.
+	Leadwerks::World*					m_pWorld;												// The main world handle for the application.
+	Leadwerks::Camera*					m_pCamera;												// The main camera handle for the application.
+
+	std::string							m_name;													// The name of the application
+	unsigned							m_nScreenWidth;											// The current width, in pixels, of the screen.
+	unsigned							m_nScreenHeight;										// The current height, in pixels, of the screen
+
+	bool								m_bPaused;												// Indicates if the application is paused.
+	bool								m_bFullscreen;											// Indicates if the application is rendered in full-screen mode.
+	bool								m_bSettingsChangedThisFrame;							// Indicates if the application's settings were changed this frame.
+	bool								m_bStateChangedThisFrame;								// Indicates whether the application experienced a change in state, this frame.
+
+}; // end class App.
+
+/* Externals */
+extern App* gApp;
+
+// -----
+
+#endif // _APP_H_
