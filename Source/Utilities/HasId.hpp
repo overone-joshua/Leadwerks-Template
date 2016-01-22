2289c719f992a03609683a5580b968c22e03ac2a
Joshua Allen <Joshua(at)edensoftworks(dot)net>
2016-01-22 13:27:40 -0500

    Created HasId class, implementing IHasId interface.

---
+++ Source/Utilities/HasId.hpp
@@ -0,0 +1,28 @@
+#ifndef _HASID_HPP_
+	#define _HASID_HPP_
+	
+template <typename T>
+class HasId : public virtual IHasId<T> {
+public:
+
+	HasId(T _id);
+	virtual ~HasId(void);
+	
+	T Id(void);
+
+private:
+
+	T m_id;
+
+}; // end class.
+	
+template <typename T>
+T HasId<T>::Id(void) { return m_id; }
+	
+template <typename T>
+HasId<T>::HasId(T _id) : m_id(_id)) { }
+	
+template <typename T>
+HasId<T>::~HasId(void) { }
+	
+#endif _HASID_HPP_
