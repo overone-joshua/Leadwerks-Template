f5f5bb81b372ed9a1e9dc4061cb84d6d18307a4b
Joshua Allen <Joshua(at)edensoftworks(dot)net>
2016-01-23 23:23:06 -0500

    Added Manager class for type Actor to ActorMgr class.

--- Source/Services/ActorMgr.cpp
+++ Source/Services/ActorMgr.cpp
@@ -1,13 +1,30 @@
 #pragma once
 #include "ActorMgr.hpp"
+#include "../Utilities/Macros.hpp"
 
-ActorMgr::ActorMgr(void) { }
+ActorMgr::ActorMgr(void) : m_nRunningIndex(0) {	
+	m_pActorManager = new Manager<Actor>();
+}
 
-ActorMgr::~ActorMgr(void) { }
+ActorMgr::~ActorMgr(void) {	
+	SAFE_DELETE(m_pActorManager);
+}
 
 long ActorMgr::GenerateId(void) { 
 	
 	m_nRunningIndex += 1; 
 	
 	return m_nRunningIndex; 
+}
+
+Actor* ActorMgr::Fetch(long _id) {
+	return m_pActorManager->Fetch(_id);
+}
+
+Actor* ActorMgr::Create(void) {
+	return m_pActorManager->Create(GenerateId());
+}
+
+void ActorMgr::Delete(long _id) {
+	m_pActorManager->Delete(_id);
 }
--- Source/Services/ActorMgr.hpp
+++ Source/Services/ActorMgr.hpp
@@ -2,18 +2,31 @@
 	#define _ACTOR_MANAGER_HPP_
 	
 #pragma once
+#include "Manager.hpp"
+#include "../Objects/Actor.hpp"
+
+#include <map>
 
 class ActorMgr {
 public:
 
 	ActorMgr(void);
-	~ActorMgr(void);
+	~ActorMgr(void);	
 
+	Actor* Fetch(long _id);
+
+	Actor* Create(void);
+
+	void Delete(long _id);
+	
+protected:
 	long GenerateId(void);
 
-private:
+private:	
 
 	long m_nRunningIndex;
+
+	Manager<Actor>* m_pActorManager;
 
 }; // < end class.
 
