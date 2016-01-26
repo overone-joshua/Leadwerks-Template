88243701845ebe6e625fb76e72e97370c6579972
Joshua Allen <Joshua(at)edensoftworks(dot)net>
2016-01-26 16:35:30 -0500

    Added IHasId and HasId source files to Common.hpp. Added typedef for uint64_t.

--- Source/Common.hpp
+++ Source/Common.hpp
@@ -8,10 +8,18 @@
 #pragma once
 #include "Leadwerks.h"
 
+#include <cstdint>
+
+#include "Utilities/IHasId.hpp"
+#include "Utilities/HasId.hpp"
+
 #include "App.h"
 #include "Services/AppCtrl.hpp"
 
 extern App* gApp;
 extern AppCtrl* gAppCtrl;
+
+typedef std::uint32_t uint32_t;
+typedef std::uint64_t uint64_t;
 
 #endif _COMMON_HPP_H
