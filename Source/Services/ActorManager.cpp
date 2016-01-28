881bc3de08a749a9fdd34a3dfc03c7c33f20a26f
Joshua Allen <Joshua(at)edensoftworks(dot)net>
2016-01-27 21:31:27 -0500

    Rewrote ActorManager Fetch, Create and Delete methods.

--- Source/Services/ActorManager.cpp
+++ Source/Services/ActorManager.cpp
@@ -1,27 +1,62 @@
 #pragma once
 #include "../Common.hpp"
 #include "ActorManager.hpp"
+#include "../Objects/Actor.hpp"
+#include "../Components/Component.hpp"
 
-ActorManager::ActorManager(void) : m_nRunningIndex(0) {	
+#include <cassert>
+
+ActorManager::ActorManager(const Factory<Component> pComponentFactory) 
+	: m_nRunningIndex(0) , m_pComponentFactory(pComponentFactory) {	
 }
 
 ActorManager::~ActorManager(void) {
+
+	auto iter = m_actors.begin();
+	while (iter != m_actors.end()) {
+		SAFE_DELETE(iter->second);
+		iter++;
+	}
+
+	m_actors.empty();
+
 }
 
-uint64_t ActorManager::GenerateId(void) {
+uint64_t ActorManager::GetNextId(void) {
 
 	m_nRunningIndex += 1;
 
 	return m_nRunningIndex;
 }
 
-Actor* ActorManager::Fetch(uint64_t _id) {	
-	return nullptr;
+
+uint64_t ActorManager::Create(void) {	
+
+	uint64_t index = this->GetNextId();	
+	Actor* actor = new Actor();
+	actor->setId(index);
+
+	auto newActor = std::pair<uint64_t, Actor*>(index, actor);
+
+	m_actors.insert(newActor);
+	return index;
+
 }
 
-Actor* ActorManager::Create(void) {	
-	return nullptr;
+void ActorManager::Delete(uint64_t _id) {
+
+	auto it = m_actors.find(_id);
+	if (it != m_actors.end()) { 
+		SAFE_DELETE(it->second);
+		m_actors.erase(it); 
+	}
+
 }
 
-void ActorManager::Delete(uint64_t _id) {	
+const Actor* ActorManager::Fetch(uint64_t _id) {	
+	
+	auto it = m_actors.find(_id);
+	if (it != m_actors.end()) { return it->second; }
+	
+	return nullptr;
 }
