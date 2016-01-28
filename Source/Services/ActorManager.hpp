8b2a377562ba5c4cea425a370da035a0af1ee771
Joshua Allen <Joshua(at)edensoftworks(dot)net>
2016-01-27 21:30:21 -0500

    Added typedef for ActorComponentKey, ActorComponentMap and ActorComponentKey Comparer.

--- Source/Services/ActorManager.hpp
+++ Source/Services/ActorManager.hpp
@@ -2,15 +2,26 @@
 	#define _ACTOR_MANAGER_HPP_
 	
 #pragma once
-#include "../Common.hpp"
 #include "../Utilities/Macros.hpp"
+#include "../Components/Component.hpp"
+#include "../Common.hpp"
+#include <cassert>
+#include <map>
+#include <vector>
 
 class Actor;
 
-class ActorManager {
+class ActorManager {	
+	template <typename T1, typename T2>
+	struct ActorComponentKeyComparer {
+		bool operator () (const std::pair<T1, T2>& x, const std::pair<T1, T2>& y) {
+			return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
+		}
+	};
+	typedef std::pair<uint64_t, const char*> ActorComponentKey;
+	typedef std::map<ActorComponentKey, std::vector<const Component*>, ActorComponentKeyComparer<uint64_t, const char*>> ActorComponentMap;
 public:
 
-	ActorManager(void);
 	~ActorManager(void);	
 
 	Actor* Fetch(uint64_t _id);
