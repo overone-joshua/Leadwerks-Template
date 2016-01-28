5d0f7ae99da433570886a3843acf633df61d7d11
Joshua Allen <Joshua(at)edensoftworks(dot)net>
2016-01-27 21:31:55 -0500

    Removed ambiguous headers from common.hpp.

--- Source/Common.hpp
+++ Source/Common.hpp
@@ -6,15 +6,11 @@
     #define _COMMON_HPP_H
 
 #pragma once
-#include "Leadwerks.h"
-
 #include <cstdint>
-
-#include "Utilities/IHasId.hpp"
-#include "Utilities/HasId.hpp"
 
 #include "App.h"
 #include "Services/AppCtrl.hpp"
+
 
 extern App* gApp;
 extern AppCtrl* gAppCtrl;
