6a308a129f388208fb76c634673f1a21e4533ecc
Joshua Allen <Joshua(at)edensoftworks(dot)net>
2016-01-28 21:06:43 -0500

    Updated AddComponent method and added RemoveComponent method.

--- Source/Services/ActorManager.hpp
+++ Source/Services/ActorManager.hpp
@@ -3,11 +3,13 @@
 	
 #pragma once
 #include "../Utilities/Macros.hpp"
-#include "../Components/Component.hpp"
 #include "../Common.hpp"
+#include "../Utilities/HasId.hpp"
+#include "../Components/Component.hpp"
 
 #include <cassert>
 #include <map>
+#include <string>
 #include <vector>
 
 class Actor;
@@ -17,12 +19,12 @@ class ActorManager {
 	template <typename T1, typename T2>
 	struct ActorComponentKeyComparer {
 		bool operator () (const std::pair<T1, T2>& x, const std::pair<T1, T2>& y) {
-			return x.first < y.first || (!(y.first < x.first) && x.second < y.second);
+			return x.first < y.first && x.second < y.second;
 		}
 	};
 
-	typedef std::pair<uint64_t, const char*> ActorComponentKey;
-	typedef std::map<ActorComponentKey, std::vector<const Component*>, ActorComponentKeyComparer<uint64_t, const char*>> ActorComponentMap;
+	typedef std::pair<uint64_t, std::string> ActorComponentKey;
+	typedef std::map<ActorComponentKey, const Component*, ActorComponentKeyComparer<uint64_t, std::string>> ActorComponentMap;
 
 public:
 
@@ -38,6 +40,9 @@ public:
 	template <typename T>
 	void AddComponent(uint64_t _id);
 
+	template <typename T>
+	void RemoveComponent(uint64_t _id);
+
 protected:
 	uint64_t GetNextId(void);
 
@@ -46,27 +51,46 @@ private:
 	uint64_t m_nRunningIndex;
 
 	std::map<uint64_t, Actor*> m_actors;	
-
-	//const ActorComponentMap m_actorComponents;
+	
 	ActorComponentMap m_actorComponents;
 
 }; // < end class.
 
 template <typename T>
-void ActorManager::AddComponent(uint64_t _id) {
+void ActorManager::AddComponent(uint64_t _id) {	
 
 	// < Are we attempting to add a component to an non-existing
 	// * actor?
 	auto actor = this->Fetch(_id);
 	assert(actor != nullptr);
+	
+	std::string type = T::ClassType();
+	auto key = std::make_pair(_id, type);
 
-	auto type = T::ClassType();
 	T* newComponent = (T*)(gComponentFactory.Create(type));
 	newComponent->setId(_id);
 
-	auto actorComponents = m_actorComponents[make_pair(_id, type)];
-	actorComponents.push_back(newComponent);
+	auto actorComponent = m_actorComponents.insert(std::make_pair(key, newComponent));
 
+}
+
+template <typename T>
+void ActorManager::RemoveComponent(uint64_t _id) {
+
+	auto actor = this->Fetch(_id);
+	if (actor == nullptr) { return; }
+	
+	std::string type = T::ClassType();
+	auto key = std::make_pair(_id, type);
+
+	auto iter = m_actorComponents.find(key);
+	if (iter == m_actorComponents.end()) { return; }
+
+	auto it = (iter)->second;
+	if (it != nullptr) { SAFE_DELETE(it); }
+
+	m_actorComponents.erase(iter);
+	
 }
 
 #endif _ACTOR_MANAGER_HPP_
