8a5ae5128237672ba267b0b6193d80347f750da3
Joshua Allen <Joshua(at)edensoftworks(dot)net>
2016-01-31 17:27:21 -0500

    Made gStateFactor external and moved declaration to State.cpp.

--- Source/States/State.hpp
+++ Source/States/State.hpp
@@ -28,28 +28,6 @@ public:
 
 }; // < end class.
 
-State::State(void) { }
-
-void State::Load(void) { }
-
-void State::Close(void) { }
-
-void State::preUpdate(void) { }
-
-void State::postUpdate(void) { }
-
-void State::preRender(void) { }
-
-void State::Render(void) { }
-
-void State::postRender(void) { }
-
-void State::preDraw(void) { }
-
-void State::Draw(void) { }
-
-void State::postDraw(void) { }
-
-Factory<State> gComponentFactory;
+extern Factory<State> gStateFactory;
 
 #endif _STATE_HPP_
