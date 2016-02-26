84b54f0f8eafb9256193d9246a62fee2e7eb3c54
Joshua Allen <joshua(AT)edensoftworks(DOT)net>
2016-02-25 21:12:06 -0500

    Added Dispose method to App class and added manager calls to App Update, Render and Draw methods.

--- Source/App.cpp
+++ Source/App.cpp
@@ -5,11 +5,16 @@
 #include "Common.hpp"
 #include "Utilities/Macros.hpp"
 
+#include "Utilities/Container.hpp"
+
 #include "Utilities/WindowHandle.hpp"
 #include "Utilities/ContextHandle.hpp"
-#include "Utilities/Container.hpp"
+
 #include "Managers/EventManager.hpp"
 #include "Managers/InputManager.hpp"
+#include "Managers/StateManager.hpp"
+
+#include "States/DefaultState.hpp"
 
 App::App(void) 
 	: m_pEventManager(nullptr), m_pInputManager(nullptr) { }
@@ -18,38 +23,116 @@ App::~App(void) { }
 
 void App::Configure(Container* pContainer) {
 
+	/* StateManager */
+	m_pStateManager = pContainer->Register<StateManager, StateManager>( new StateManager());
+
+	/* EventManager */
 	m_pEventManager = pContainer->Register<EventManager, EventManager>( new EventManager());
 
+	/* Input Manager */
 	m_pInputManager = pContainer->Register<InputManager, InputManager>( new InputManager(
 		pContainer->Resolve<WindowHandle>()->getInst(), 
 		pContainer->Resolve<ContextHandle>()->getInst(),
 		pContainer->Resolve<EventManager>()));
 
+	gStateFactory.Register(new FactoryMaker<DefaultState, State>);
 }
 
 bool App::Start(void) {
+
+	// < Add our default state and immediately make it active.
+	m_pStateManager->AddState<DefaultState>(true);
 
 	std::cout << "Application initialization completed successfully. \n";
 	
 	return true;
 }
 
-void App::Shutdown(void) { }
+void App::Shutdown(void) { Dispose(); }
 
-void App::preUpdate(void) { }
+void App::Dispose(void) 
+{
+	gStateFactory.Unregister(DefaultState::ClassType());
+}
 
-bool App::Update(float dt) { return true; }
+void App::preUpdate(void) 
+{ 
+	// < Call the StateManager's preUpdate.
+	m_pStateManager->preUpdate();
 
-void App::postUpdate(void) { }
+}
 
-void App::preRender(void) { }
+bool App::Update(float dt) 
+{ 
+	// < Call EventManager's update. We allow it to process events 
+	// * for 200ms a frame.
+	m_pEventManager->Update(200);
+	
+	// < Call the InputManager's update.
+	m_pInputManager->Update(dt);
 
-void App::Render(void) { }
+	// < Call the StateManager's Update.
+	m_pStateManager->Update(dt);
 
-void App::postRender(void) { }
+	return true; 
 
-void App::preDraw(void) { }
+}
 
-void App::Draw(void) { }
+void App::postUpdate(void) 
+{ 
+	// < Call the InputManager's postUpdate.
+	m_pStateManager->postUpdate();
 
-void App::postDraw(void) { }
+}
+
+void App::preRender(void) 
+{
+	// < Call the StateManager's preRender.
+	m_pStateManager->preRender();
+
+}
+
+void App::Render(void)
+{
+	// < Perform all 3D-Rendering here.
+	// < ---
+
+	// < Call the StateManager's Render.
+	m_pStateManager->Render();
+
+	// < ---
+
+}
+
+void App::postRender(void) 
+{
+	// < Call the StateManager's postRender.
+	m_pStateManager->postRender();
+
+}
+
+void App::preDraw(void) 
+{
+	// < Call the StateManager's preDraw.
+	m_pStateManager->preDraw();
+
+}
+
+void App::Draw(void) 
+{
+	// < Perform all 2D-Rendering here.
+	// < ---
+
+	// < Call the StateManager's Draw.
+	m_pStateManager->Draw();
+
+	// < ---
+
+}
+
+void App::postDraw(void) 
+{
+	// < Call the StateManager's postDraw.
+	m_pStateManager->postDraw();
+
+}
--- Source/App.h
+++ Source/App.h
@@ -32,6 +32,10 @@ public:
 	void 			postDraw	(void);
 	void 			Draw		(void);
 
+protected:
+
+	void			Dispose		(void);
+
 private:
 
 	EventManager* m_pEventManager;
