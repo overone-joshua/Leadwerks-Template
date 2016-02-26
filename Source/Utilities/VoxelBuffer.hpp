ea08f79fd4a0896bf4c42ad216479c576a0a4bb3
Joshua Allen <joshua(AT)edensoftworks(DOT)net>
2016-02-25 22:37:08 -0500

    Added utility VoxelBuffer to project.

---
+++ Source/Utilities/VoxelBuffer.hpp
@@ -0,0 +1,69 @@
+#ifndef _VOXEL_BUFFER_H
+#define _VOXEL_BUFFER_H_
+
+#pragma once
+#include <vector>
+
+template <typename T>
+struct VoxelBuffer {
+public:
+	VoxelBuffer(int nSizeX, int nSizeY, int nSizeZ)
+		: m_nSizeX(nSizeX), m_nSizeY(nSizeY), m_nSizeZ(nSizeZ) {
+
+		m_data.resize(nSizeX * nSizeY * nSizeZ);
+	}
+
+	~VoxelBuffer() {
+		m_data.clear();
+	}
+
+	T Get(int i, int j, int k) {
+		return m_data[Index(i, j, k)];
+	}
+
+	T Get(int index) {
+		return m_data[index];
+	}
+
+	void Set(int i, int j, int k, T val) {
+		m_data[Index(i, j, k)] = val;
+	}
+
+	void Set(int index, T val) {
+		m_data[index] = val;
+	}
+
+	void Load(const char* filename) {
+		std::ifstream in(filename);
+		in >> m_nSizeX >> m_nSizeY >> m_nSizeZ;
+		std::copy(std::istream_iterator<T>(in),
+			std::istream_iterator<T>(),
+			std::back_inserter(m_data));
+	}
+
+	void Save(const char* filename) {
+		std::ofstream out(filename);
+		out << m_nSizeX << " " << m_nSizeY << " " << m_nSizeZ << std::endl;
+		std::copy(m_data.begin(), m_data.end(), std::ostream_iterator<T>(out, "\n"));
+
+	}
+
+	operator T*() {
+		return &m_data[0];
+	}
+
+protected:
+
+private:
+	int Index(int i, int j, int k) {
+		return i + j * m_nSizeX + k * m_nSizeX * m_nSizeY;
+	}
+
+	int m_nSizeX;
+	int m_nSizeY;
+	int m_nSizeZ;
+	std::vector<T> m_data;
+
+}; // end struct VoxelBuffer<T>
+
+#endif // _VOXEL_BUFFER_H_
