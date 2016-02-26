c415c7c973441ab5284d5aa5438cb88687e7636a
Joshua Allen <joshua(AT)edensoftworks(DOT)net>
2016-02-26 17:35:20 -0500

    Base State class now supports a Configure method giving it access to the container.

--- Source/States/State.cpp
+++ Source/States/State.cpp
@@ -2,25 +2,3 @@
 #include "State.hpp"
 
 Factory<State> gStateFactory;
-
-State::State(void) { }
-
-void State::Load(void) { }
-
-void State::Close(void) { }
-
-void State::preUpdate(void) { }
-
-void State::postUpdate(void) { }
-
-void State::preRender(void) { }
-
-void State::Render(void) { }
-
-void State::postRender(void) { }
-
-void State::preDraw(void) { }
-
-void State::Draw(void) { }
-
-void State::postDraw(void) { }
--- Source/States/State.hpp
+++ Source/States/State.hpp
@@ -2,6 +2,7 @@
 	#define _STATE_HPP_
 
 #pragma once
+#include "../Utilities/Container.hpp"
 #include "../Utilities/Factory.hpp"
 
 class State {
@@ -9,22 +10,23 @@ public:
 
 	virtual const char* ObjectType(void) = 0;	
 
-	State(void);
+	State(void) { }
 
-	virtual void Load(void);
-	virtual void Close(void);
+	virtual void Configure(const Container* pContainer) const = 0;
+	virtual void Load(void) { }
+	virtual void Close(void) { }
 
-	virtual void preUpdate(void);
-	virtual void postUpdate(void);
+	virtual void preUpdate(void) { }
+	virtual void postUpdate(void) { }
 	virtual bool Update(float deltaTime) = 0;
 
-	virtual void preRender(void);
-	virtual void postRender(void);
-	virtual void Render(void);
+	virtual void preRender(void) { }
+	virtual void postRender(void) { }
+	virtual void Render(void) { }
 
-	virtual void preDraw(void);
-	virtual void postDraw(void);
-	virtual void Draw(void);
+	virtual void preDraw(void) { }
+	virtual void postDraw(void) { }
+	virtual void Draw(void) { }
 
 }; // < end class.
 
