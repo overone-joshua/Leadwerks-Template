400625eadc5120e642353e086bd832d83927cb2a
Joshua Allen <joshua(AT)edensoftworks(DOT)net>
2016-02-28 10:56:28 -0500

    Removed all source for previous component system.

--- Source/Components/Component.cpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Component.hpp"
-
-Factory<Component> gComponentFactory;
-
-Component::Component(void) { }
-
-Component::~Component(void) { }
--- Source/Objects/Actor.cpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "Actor.hpp"
-#include "../Utilities/HasId.hpp"
-
-Actor::Actor(void) { }
-
-Actor::~Actor(void) { }
