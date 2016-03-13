9218856e733d32b82c49b076e6815f9bd54a25d2
Joshua Allen <joshua(AT)edensoftworks(DOT)net>
2016-03-12 22:07:40 -0400

    Created sample script files for the game camera and the crawler model.

---
+++ Scripts/Camera.lua
@@ -0,0 +1,11 @@
+-- camera.lua
+
+Camera = 
+{
+    name = "Camera",    
+    pos = { 0, 2, 0 },
+    rot = { 0, 0, 0 },
+    sca = { 1, 1, 1 }
+}
+
+return Camera;
---
+++ Scripts/Crawler.lua
@@ -0,0 +1,12 @@
+-- Crawler.lua
+
+Crawler = 
+{
+    name = "Crawler",    
+    modelPath = "./Models/Characters/Crawler/crawler.mdl",        
+    pos = { 0, 0, 0 },
+    rot = { 0, 0, 0 },
+    sca = { 1, 1, 1 }    
+}
+
+return Crawler;
