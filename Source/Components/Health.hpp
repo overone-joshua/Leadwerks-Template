3a24f947b705ef5cef616dfd481fe8a9e78606f8
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-15 13:08:38 -0400

    Created generic Health component.
    The health component is used to keep track of an entity's current health value. The component is generic in order to provide flexability in order to allow non-numeric health representations.

---
+++ Source/Components/Health.hpp
@@ -0,0 +1,45 @@
+/*-------------------------------------------------------
+                    <copyright>
+    
+    File: Health.hpp
+    Language: C++
+    
+    (C) Copyright Eden Softworks
+    
+    Author: Joshua Allen
+    E-Mail: Joshua(AT)EdenSoftworks(DOT)net
+    
+    Description: Header file for Health component.
+
+---------------------------------------------------------*/
+
+#ifndef _HEALTH_COMPONENT_HPP_
+    #define _HEALTH_COMPONENT_HPP_
+    
+#pragma once
+#include "../Utilities/Macros.hpp"
+
+#include "Component.hpp"
+
+#include <string>
+
+namespace component
+{
+    // < A health component.
+    // * The health component provides access to health-related properties.
+    template <typename T>
+    typedef struct Health : public Component
+    {
+        CLASS_TYPE(Health);
+        
+        T               nCur;       /*!< The current health value of type T. >/      
+        
+        /** The Health component constructor. */
+        Health(T _nHealth = 0, std::string cName = "") 
+            : nCur(nHealth), Component(cName) { }
+        
+    } Health; // < end struct.
+    
+} // < end namespace.
+
+#endif _HEALTH_COMPONENT_HPP_
