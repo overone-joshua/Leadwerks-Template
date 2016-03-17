107d413f289c11bb331001c4d58c0f6bfa24111b
Joshua Allen <joshua(AT)edensoftworks(DOT)net>
2016-03-16 21:37:51 -0400

    Updated World component with Add, Get and GetAll methods.
    Each added method allows a developer to write theire own implementation in order to handle added custom Entities to the world through a subset of components and access them later.

--- Source/Components/World.hpp
+++ Source/Components/World.hpp
@@ -61,6 +61,11 @@ namespace Components
 				
 		template <typename T> std::vector<T>*             GetComponents(World* pWorld, uint64_t entity);                          /** Returns a Component collection of type T assocated with the given entity. */
 
+
+        template <typename T> static uint64_t Add(World& world, T& source);
+        template <typename Out> static Out Get(World& world, uint64_t entity);
+        template <typename Out> static std::vector<Out> GetAll(World& world);
+
 		uint64_t& operator [] (int index)
 		{
 			return m_entityMasks[index];
