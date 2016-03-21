5dee72f412da48f364ebfdcf160c6a8e44505ddb
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-21 13:58:08 -0400

    Updated base Actor entity.

--- Source/Entities/Actor.cpp
+++ Source/Entities/Actor.cpp
@@ -1,79 +1,170 @@
 #pragma once
 #include "Actor.hpp"
 
-// < Convert our lua Actor script into an ActorHandle object.
-template <> ActorHandle TypeConverter::Convert<LuaTable, ActorHandle>(LuaTable& source)
+namespace Entities
 {
-	ActorHandle hndl;
+
+    uint64_t Actor::Create(Components::World& world, std::string cScriptPath)
+    {
+        // < Load the script containing our actor information.
+        auto table = LuaTable::fromFile(cScriptPath.c_str());
+
+        // < Convert our actor script into an actor handle.
+        Actor hndl = TypeConverter::Convert<LuaTable, Actor>(table);
+
+        // < Add the entity to the world.
+        auto entity = Components::World::Add(world, hndl);
+
+        return entity;
+    }
+
+    void Actor::Load(Components::World& world, uint64_t entity)
+    {
+        auto res = Components::World::Get<Actor>(world, entity);
+
+        // < Create the actor's model. 
+        res.models.push_back(Leadwerks::Model::Load(res.appearanceComponents.front().cModelPath));
+        res.models.front()->SetScale(res.placementComponents.front().vSca);
+        res.models.front()->SetRotation(res.placementComponents.front().vRot, false);
+        res.models.front()->SetPosition(res.placementComponents.front().vPos, true);
+    }
+
+    void Actor::LoadAll(Components::World& world)
+    {
+        auto entities = Components::World::GetAll<Actor>(world);
+        auto iter = entities.begin();
+        while (iter != entities.end())
+        {
+            auto hndl = (*iter).models;
+            // < Perform initialization here.
+            // * ---                                        
+
+            // < Create the actor's model. 
+            hndl.push_back(Leadwerks::Model::Load((*iter).appearanceComponents.front().cModelPath));
+            hndl.front()->SetScale((*iter).placementComponents.front().vSca);
+            hndl.front()->SetRotation((*iter).placementComponents.front().vRot, false);
+            hndl.front()->SetPosition((*iter).placementComponents.front().vPos, true);
+
+            // < ---
+
+            iter++;
+
+        }
+    }
+
+    void Actor::Close(Components::World& world, uint64_t entity)
+    {
+        
+    }
+
+    void Actor::CloseAll(Components::World& world)
+    {
+        auto entities = Components::World::GetAll<Actor>(world);
+        auto iter = entities.begin();
+        while (iter != entities.end())
+        {
+            auto hndl = (*iter).models;
+
+            auto it = hndl.begin();
+            while (it != hndl.end())
+            {
+                if ((*it) != nullptr)
+                {
+                    auto model = (*it);
+                    SAFE_RELEASE(model);
+                    SAFE_DELETE(model);
+                }
+
+                it++;
+            }
+            hndl.clear();
+
+            iter++;
+
+        }
+        entities.clear();
+    }
+
+    void Actor::Update(InputManager* pInputMgr, Components::World& world, uint64_t entity, float dt)
+    {
+        auto entities = Components::World::GetAll<Actor>(world);
+    }
+
+} // < end namespace.
+
+// < Convert our lua Actor script into an Actor object.
+template <> Entities::Actor TypeConverter::Convert<LuaTable, Entities::Actor>(LuaTable& source)
+{
+	Entities::Actor hndl;
 
 	// < Extract all required actor information.
 	auto name = source["name"].get<std::string>();
 	auto vPos = source["pos"].get<Leadwerks::Vec3>();
 	auto vRot = source["rot"].get<Leadwerks::Vec3>();
 	auto vSca = source["sca"].get<Leadwerks::Vec3>();
+    auto vMovSpeed = source["movSpeed"].get<Leadwerks::Vec3>();
+    auto vRotSpeed = source["rotSpeed"].get<Leadwerks::Vec3>();
 	auto path = source["modelPath"].get<std::string>();
 	auto vVel = Leadwerks::Vec3(0.0f, 0.0f, 0.0f);
 
 	hndl.component = Components::Component(name);
-	hndl.appearance.push_back(Components::Appearance(path, name));
-	hndl.input.push_back(Components::Input(name));
-	hndl.placement.push_back(Components::Placement(vPos, vRot, vSca, name));
-	hndl.velocity.push_back(Components::Velocity(vVel, name));
+	hndl.appearanceComponents.push_back(Components::Appearance(path, name));
+	hndl.inputComponents.push_back(Components::Input<Entities::Actor>(vMovSpeed, vRotSpeed, name));
+	hndl.placementComponents.push_back(Components::Placement(vPos, vRot, vSca, name));
+	hndl.velocityComponents.push_back(Components::Velocity(vVel, name));
 
 	return hndl;
 }
 
 // < Add our actor handle object to our world as its individual components.
-template <> uint64_t Components::World::Add<ActorHandle>(Components::World& world, ActorHandle& source)
-{
-    using namespace Entities;
-
+template <> uint64_t Components::World::Add<Entities::Actor>(Components::World& world, Entities::Actor& source)
+{    
     // < Create a new entity of type Actor.
     auto entity = world.CreateEntity(&world);
-    world.Get(entity) = MASK_ACTOR;
+    world.Get(entity) = Entities::MASK_ACTOR;
 
     // < Create the components required to form an Actor.                        
-    world.AddComponent(&world, entity, source.appearance.front());
-    world.AddComponent(&world, entity, source.input.front());
-    world.AddComponent(&world, entity, source.placement.front());
-    world.AddComponent(&world, entity, source.velocity.front());
+    world.AddComponent(&world, entity, source.appearanceComponents.front());
+    world.AddComponent(&world, entity, source.inputComponents.front());
+    world.AddComponent(&world, entity, source.placementComponents.front());
+    world.AddComponent(&world, entity, source.velocityComponents.front());
 
     return entity;
 }
 
 // < Get a reference to all our entities components by getting the equivilent
-// * ActorHandle from the world.
-template <> ActorHandle Components::World::Get<ActorHandle>(Components::World& world, uint64_t entity)
-{
-    using namespace Entities;
-    ActorHandle hndl;
+// * Actor from the world.
+template <> Entities::Actor Components::World::Get<Entities::Actor>(Components::World& world, uint64_t entity)
+{    
+    Entities::Actor hndl;
 
     auto appearanceComponents = world.GetComponents<Components::Appearance>(&world, entity);
-    auto inputComponents = world.GetComponents<Components::Input>(&world, entity);
+    auto inputComponents = world.GetComponents<Components::Input<Entities::Actor>>(&world, entity);
     auto placementComponents = world.GetComponents<Components::Placement>(&world, entity);
     auto velocityComponents = world.GetComponents<Components::Velocity>(&world, entity);
+    
+    hndl.appearanceComponents = *appearanceComponents;
+    hndl.inputComponents = *inputComponents;
+    hndl.placementComponents = *placementComponents;
+    hndl.velocityComponents = *velocityComponents;
 
-    hndl.component = Components::Component();
-    hndl.appearance = *appearanceComponents;
-    hndl.input = *inputComponents;
-    hndl.placement = *placementComponents;
-    hndl.velocity = *velocityComponents;
+    hndl.component = Components::Component(appearanceComponents->front().cName);
+    hndl.component.nId = entity;
 
     return hndl;
 }
 
 // < Get a reference to all actors from the world.
-template <> std::vector<ActorHandle> Components::World::GetAll<ActorHandle>(Components::World& world)
-{
-    using namespace Entities;
-    std::vector<ActorHandle> results;
+template <> std::vector<Entities::Actor> Components::World::GetAll<Entities::Actor>(Components::World& world)
+{    
+    std::vector<Entities::Actor> results;
 
-    auto entities = world.GetEntities(&world, MASK_ACTOR);
+    auto entities = world.GetEntities(&world, Entities::MASK_ACTOR);
 
     auto iter = entities.begin();
     while (iter != entities.end())
     {
-        results.push_back(Components::World::Get<ActorHandle>(world, (*iter)));
+        results.push_back(Components::World::Get<Entities::Actor>(world, (*iter)));
         iter++;
     }
 
--- Source/Entities/Actor.hpp
+++ Source/Entities/Actor.hpp
@@ -19,10 +19,14 @@
     
 #pragma once
 #include "Leadwerks.h"
-#include "../Utilities/Macros.hpp"
 #include "../Managers/InputManager.hpp"
+
+#include "../Common.hpp"
+#include "../Utilities/Macros.hpp"
+#include "../Utilities/Event.hpp"
+#include "../Utilities/luatables/luatables.h"
+
 #include "../Mapping/TypeConverter.hpp"
-#include "Entity.hpp"
 
 #include "../Components/Component.hpp"
 #include "../Components/ComponentDictionary.hpp"
@@ -32,110 +36,56 @@
 #include "../Components/Placement.hpp"
 #include "../Components/Velocity.hpp"
 
-#include "../Utilities/luatables/luatables.h"
+#include "Entity.hpp"
 
 #include <string>
 #include <vector>
 
-typedef struct ActorHandle
-{
-	Components::Component                      component;
-	std::vector<Components::Appearance>        appearance;
-	std::vector<Components::Input>             input;
-	std::vector<Components::Placement>         placement;
-	std::vector<Components::Velocity>          velocity;
-	std::vector<Leadwerks::Model*>              models;
-
-} ActorHandle; // < end struct.
-
-template <> ActorHandle TypeConverter::Convert<LuaTable, ActorHandle>(LuaTable& source);
-
-template <> uint64_t Components::World::Add<ActorHandle>(Components::World& world, ActorHandle& source);
-template <> ActorHandle Components::World::Get<ActorHandle>(Components::World& world, uint64_t entity);
-template <> std::vector<ActorHandle> Components::World::GetAll<ActorHandle>(Components::World& world);
-
-
 namespace Entities
 {
-    const uint64_t MASK_ACTOR = COMPONENT_APPEARANCE | COMPONENT_PLACEMENT | COMPONENT_INPUT | COMPONENT_VELOCITY;       
+    const uint64_t MASK_ACTOR = COMPONENT_APPEARANCE | COMPONENT_PLACEMENT | COMPONENT_INPUT | COMPONENT_VELOCITY;           
 
-    class Actor
+    class Actor : public Entity<Actor>
     {            
     public:
 
-        static inline uint64_t Create(Components::World& world, std::string cScriptPath)
-        {            
-            // < Load the script containing our actor information.
-            auto table = LuaTable::fromFile(cScriptPath.c_str());
-            
-            // < Convert our actor script into an actor handle.
-            ActorHandle hndl = TypeConverter::Convert<LuaTable, ActorHandle>(table);                       
+        Components::Component                      component;
+        std::vector<Components::Appearance>        appearanceComponents;
+        std::vector<Components::Input<Actor>>       inputComponents;
+        std::vector<Components::Placement>         placementComponents;
+        std::vector<Components::Velocity>          velocityComponents;
+        std::vector<Leadwerks::Model*>             models;
 
-            // < Add the entity to the world.
-            auto entity = Components::World::Add(world, hndl);                                   
-            
-            return entity;
-        }
+        static uint64_t Create(Components::World& world, std::string cScriptPath = "");
         
-        static void Load(Components::World& world)
-        {
-            auto entities = Components::World::GetAll<ActorHandle>(world);
-            auto iter = entities.begin();
-            while (iter != entities.end())
-            {
-                auto hndl = (*iter);
-                // < Perform initialization here.
-                // * ---                                        
+        static void Load(Components::World& world, uint64_t entity);
+        static void LoadAll(Components::World& world);
 
-                // < Create the actor's model. 
-                hndl.models.push_back(Leadwerks::Model::Load(hndl.appearance.front().cModelPath));
-                hndl.models.front()->SetScale(hndl.placement.front().vSca);
-                hndl.models.front()->SetRotation(hndl.placement.front().vRot, false);
-                hndl.models.front()->SetPosition(hndl.placement.front().vPos, true);
+        static void Close(Components::World& world, uint64_t entity);
+        static void CloseAll(Components::World& world);
 
-                // < ---
-
-                iter++;
-
-            }
-
-        }
-
-        static void Close(Components::World& world)
-        {
-            auto entities = Components::World::GetAll<ActorHandle>(world);
-            auto iter = entities.begin();
-            while (iter != entities.end())
-            {
-                auto hndl = (*iter);
-
-                auto it = hndl.models.begin();
-                while (it != hndl.models.end())
-                {
-                    if ((*it) != nullptr)
-                    {
-                        auto model = (*it);
-                        SAFE_RELEASE(model);
-                        SAFE_DELETE(model);
-                    }
-                    
-                    it++;
-                }
-                hndl.models.clear();
-
-                iter++;
-
-            }
-            entities.clear();
-        }
-
-        static void Update(InputManager* pInputMgr, Components::World& world, float dt)
-        {
-            auto entities = Components::World::GetAll<ActorHandle>(world);
-        }        
+        static void Update(InputManager* pInputMgr, Components::World& world, uint64_t entity, float dt);
         
+        static void OnKeyDown(Components::World& world, uint64_t entity, Event_KeyDown* pEvent);
+
+        static void OnKeyUp(Components::World& world, uint64_t entity, Event_KeyUp* pEvent);
+
+    protected:
+
+        void OnKeyDown(Components::World& world, Event_KeyDown* pData);        
+
+        void OnKeyUp(Components::World& world, Event_KeyUp* pData);
+
     }; // < end class.
     
 } // < end namespace.
+
+template <> Entities::Actor TypeConverter::Convert<LuaTable, Entities::Actor>(LuaTable& source);
+
+template <> void Components::World::Add<Entities::Actor>(Components::World& world, uint64_t entity, Entities::Actor& source);
+
+template <> Entities::Actor Components::World::Get<Entities::Actor>(Components::World& world, uint64_t entity);
+
+template <> std::vector<Entities::Actor> Components::World::GetAll<Entities::Actor>(Components::World& world);
 
 #endif _ACTOR_ENTITY_HPP_
