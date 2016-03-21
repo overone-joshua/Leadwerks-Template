caf75cc843ee975a7c67176b1f54c19f2268aae4
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-21 13:58:53 -0400

    Created SpectatorCamera entity to contain all business logic for a spectator camera.

---
+++ Source/Entities/Custom/SpectatorCamera.hpp
@@ -0,0 +1,165 @@
+#ifndef _SPECTATOR_CAMERA_ENTITY_HPP_
+	#define _SPECTATOR_CAMERA_ENTITY_HPP_
+
+#pragma once
+#include "Leadwerks.h"
+#include "../../Common.hpp"
+#include "../../Utilities/Event.hpp"
+
+#include "../Camera.hpp"
+#include "../Input.hpp"
+
+#include "../../Components/World.hpp"
+#include "../../Components/Trigger.hpp"
+#include "../../Components//Custom/MouseMoveTrigger.hpp"
+#include "../../Components//Custom/KeyDownTrigger.hpp"
+#include "../../Components//Custom/KeyUpTrigger.hpp"
+
+namespace Entities
+{
+	class SpectatorCamera
+	{		
+        CLASS_TYPE(SpectatorCamera);
+
+    public:
+
+        SpectatorCamera(void) { }
+
+	    static uint64_t Create(Components::World& world, std::string _cScriptPath)
+	    {
+            auto entity = Entities::Camera::Create(world, _cScriptPath);
+
+            // < Wire up our game logic events for each entity.
+            auto mouseMove = Components::MouseMoveTrigger<Entities::Camera>::MouseMoveEvent();
+            auto keyDown = Components::KeyDownTrigger<Entities::Camera>::KeyDownEvent();
+            auto keyUp = Components::KeyUpTrigger<Entities::Camera>::KeyUpEvent();
+
+            mouseMove.Bind<&Entities::SpectatorCamera::MouseMove>();
+            keyDown.Bind<&Entities::SpectatorCamera::KeyDown>();
+            keyUp.Bind<&Entities::SpectatorCamera::KeyUp>();
+
+            world.AddComponent(&world, entity, Components::MouseMoveTrigger<Entities::Camera>(mouseMove, "Camera"));
+            world.AddComponent(&world, entity, Components::KeyDownTrigger<Entities::Camera>(keyDown, "Camera"));
+            world.AddComponent(&world, entity, Components::KeyUpTrigger<Entities::Camera>(keyUp, "Camera"));
+
+            return entity;
+	    }
+
+        static void MouseMove(Components::World& world, Entities::Camera entity, Event_MouseMove* pEvent)
+        {
+            // < Check for any mouse movement. If there is any movement, we should
+            // * look to rotate the camera.
+            auto iter = entity.inputComponents->begin();
+            while (iter != entity.inputComponents->end())
+            {
+                (*iter).vDelta = pEvent->Delta();
+
+                if (pEvent->Delta().x < 0) { (*iter).nMask |= INPUT_ROTATE_LEFT; }
+                if (pEvent->Delta().x > 0) { (*iter).nMask |= INPUT_ROTATE_RIGHT; }
+
+                if (pEvent->Delta().y < 0) { (*iter).nMask |= INPUT_ROTATE_DOWN; }
+                if (pEvent->Delta().y > 0) { (*iter).nMask |= INPUT_ROTATE_UP; }
+
+                ++iter;
+            }
+        }
+
+        static void KeyDown(Components::World& world, Entities::Camera entity, Event_KeyDown* pEvent)
+        {
+            // < Check for any key presses from the keyboard. If any key is pressed
+            // * we should look to move the camera.
+            auto iter = entity.inputComponents->begin();
+            while (iter != entity.inputComponents->end())
+            {
+                if (pEvent->Key() == Leadwerks::Key::W) { (*iter).nMask |= INPUT_MOVE_FORWARD; }
+                if (pEvent->Key() == Leadwerks::Key::A) { (*iter).nMask |= INPUT_MOVE_LEFT; }
+                if (pEvent->Key() == Leadwerks::Key::S) { (*iter).nMask |= INPUT_MOVE_BACKWARD; }
+                if (pEvent->Key() == Leadwerks::Key::D) { (*iter).nMask |= INPUT_MOVE_RIGHT; }
+
+                if (pEvent->Key() == Leadwerks::Key::E) { (*iter).nMask |= INPUT_MOVE_UP; }
+                if (pEvent->Key() == Leadwerks::Key::Q) { (*iter).nMask |= INPUT_MOVE_DOWN; }
+
+                ++iter;
+            }
+        }
+
+        static void KeyUp(Components::World& world, Entities::Camera entity, Event_KeyUp* pEvent)
+        {
+            // < Just like key press however, here we pop the movement bitmask to signal 
+            // * a key release.
+            auto iter = entity.inputComponents->begin();
+            while (iter != entity.inputComponents->end())
+            {
+                if (pEvent->Key() == Leadwerks::Key::W) { (*iter).nMask &= ~INPUT_MOVE_FORWARD; }
+                if (pEvent->Key() == Leadwerks::Key::A) { (*iter).nMask &= ~INPUT_MOVE_LEFT; }
+                if (pEvent->Key() == Leadwerks::Key::S) { (*iter).nMask &= ~INPUT_MOVE_BACKWARD; }
+                if (pEvent->Key() == Leadwerks::Key::D) { (*iter).nMask &= ~INPUT_MOVE_RIGHT; }
+
+                if (pEvent->Key() == Leadwerks::Key::E) { (*iter).nMask &= ~INPUT_MOVE_UP; }
+                if (pEvent->Key() == Leadwerks::Key::Q) { (*iter).nMask &= ~INPUT_MOVE_DOWN; }
+
+                ++iter;
+            }
+        }
+
+        static void Update(Components::World& world, uint64_t entity, float dt)
+        {
+            auto res = Components::World::Get<Entities::Camera>(world, entity);
+            auto& input = res.inputComponents->front();
+            auto& velocity = res.velocityComponents->front();
+            auto& placement = res.placementComponents->front();
+
+            // < Perform any game logic here.
+            // < ---
+            uint64_t inputMask = input.nMask;
+            float dX, dY, dZ;
+
+            // < Are we rotating the camera left or right?
+            if ((bool(inputMask & INPUT_ROTATE_RIGHT)) || (bool(inputMask & INPUT_ROTATE_LEFT))) {
+                dY = (bool(inputMask & INPUT_ROTATE_RIGHT)) - (bool(inputMask & INPUT_ROTATE_LEFT)) + input.vDelta.x * dt * input.vRotSpeed.y;
+                input.nMask &= ~(INPUT_ROTATE_RIGHT | INPUT_ROTATE_LEFT);
+            }
+
+            // < Are we tilting the camera up or down?
+            if ((bool(inputMask & INPUT_ROTATE_UP)) || (bool(inputMask & INPUT_ROTATE_DOWN))) {
+                dX = (bool(inputMask & INPUT_ROTATE_UP)) - (bool(inputMask & INPUT_ROTATE_DOWN)) + input.vDelta.y * dt * input.vRotSpeed.x;
+                input.nMask &= ~(INPUT_ROTATE_UP | INPUT_ROTATE_DOWN);
+            }
+
+            // < Are we rolling the camera left or right?
+            if ((bool(inputMask & INPUT_ROLL_RIGHT)) || (bool(inputMask & INPUT_ROLL_LEFT))) {
+                dZ = (((bool(inputMask & INPUT_ROLL_RIGHT)) - (bool(inputMask & INPUT_ROLL_LEFT))) + input.vDelta.z)  * dt * input.vRotSpeed.z;
+                input.nMask &= ~(INPUT_ROLL_LEFT | INPUT_ROLL_RIGHT);
+            }
+
+            // < Are we looking to move the camera?
+            float vX = ((bool(inputMask & INPUT_MOVE_RIGHT)) - (bool(inputMask & INPUT_MOVE_LEFT)))		* dt * input.vMovSpeed.x;
+            float vY = ((bool(inputMask & INPUT_MOVE_UP)) - (bool(inputMask & INPUT_MOVE_DOWN)))		* dt * input.vMovSpeed.y;
+            float vZ = ((bool(inputMask & INPUT_MOVE_FORWARD)) - (bool(inputMask & INPUT_MOVE_BACKWARD))) * dt * input.vMovSpeed.z;
+
+            velocity.vVel.x = vX;
+            velocity.vVel.y = vY;
+            velocity.vVel.z = vZ;
+
+            placement.vRot += Leadwerks::Vec3(dX, dY, 0.0f);
+
+            // < Set the camera's rotation and position.
+            auto& camHndl = res.cameraHandles->front().pCamHndl;
+
+            if (camHndl != nullptr)
+            {
+                auto cam = camHndl->getInst();
+
+                cam->SetRotation(Leadwerks::Vec3(
+                    Leadwerks::Math::Curve(placement.vRot.x, cam->GetRotation().x, 5.0f),
+                    Leadwerks::Math::Curve(placement.vRot.y, cam->GetRotation().y, 5.0f),
+                    Leadwerks::Math::Curve(placement.vRot.z, cam->GetRotation().z, 5.0f)), false);
+                cam->Move(velocity.vVel, true);
+            }
+        }
+
+    }; // < end class.
+
+} // < end namespace.
+
+#endif _SPECTATOR_CAMERA_ENTITY_HPP_
