d0c347708dbee4b1f687e965a993f499c5551b4b
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-21 13:36:40 -0400

    Added movSpeed and rotSpeed properties to DefaultCamera.lua.
    These properties describe the movement speed and rotation speed for the camera in all directionr.

--- Scripts/DefaultCamera.lua
+++ Scripts/DefaultCamera.lua
@@ -5,7 +5,9 @@ Camera =
     name = "Camera",    
     pos = { 0, 4, -6 },
     rot = { 0, 0, 0 },
-    sca = { 1, 1, 1 }
+    sca = { 1, 1, 1 },
+    movSpeed = { 0.075, 0.075, 0.075 },
+    rotSpeed = { 0.75, 0.75, 0.75 }
 }
 
 return Camera;
