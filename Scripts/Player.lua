9fe6dd2e7a0b8ba031deaf3d8e5ea4283fedb4c8
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-21 13:37:49 -0400

    Created script Player.lua.
    Player.lua describes the settings for a sample player.

---
+++ Scripts/Player.lua
@@ -0,0 +1,14 @@
+-- Crawler.lua
+
+Crawler = 
+{
+    name = "Player",    
+    modelPath = "./Models/Characters/Crawler/crawler.mdl",        
+    pos = { -3, 0, 3 },
+    rot = { 0, 0, 0 },
+    sca = { 3, 3, 3 },
+    movSpeed = { 0.075, 0.075, 0.075 },
+    rotSpeed = { 0.75, 0.75, 0.75 }    
+}
+
+return Crawler;
