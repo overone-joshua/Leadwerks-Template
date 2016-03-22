b315ff30e4cd54f9a879e9937045b33960068f69
Joshua Allen <joshua(AT)edensoftworks(DOT)net>
2016-03-22 06:15:13 -0400

    Created class structure for ThirdPersonCamera.

---
+++ Source/Entities/Custom/ThirdPersonCamera.hpp
@@ -0,0 +1,21 @@
+#ifndef _THIRD_PERSON_CAMERA_ENTITY_HPP_
+	#define _THIRD_PERSON_CAMERA_ENTITY_HPP_
+
+#pragma once
+#include "../../Common.hpp"
+#include "../../Utilities/Macros.hpp"
+
+#include "../../Components/World.hpp"
+
+#include "../Camera.hpp"
+
+namespace Entities
+{
+	class ThirdPersonCamera : public Entities::Camera
+	{
+
+	}; // < end class.
+
+} // < end namespace.
+
+#endif _THIRD_PERSON_CAMERA_ENTITY_HPP_
