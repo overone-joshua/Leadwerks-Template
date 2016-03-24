e5f492ea6c34fc15469f7892752a1616e8281404
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-24 11:21:50 -0400

    Created ThirdPersonCamera lua script.

---
+++ Scripts/DefaultThirdPersonCamera.lua
@@ -0,0 +1,14 @@
+-- DefaultThirdPersonCamera.lua
+
+ThirdPersonCamera = 
+{
+    name = "Camera",    
+    pos = { 0, 0, 0 },
+    rot = { 0, 0, 0 },
+    sca = { 1, 1, 1 },
+    offset = { 0, 15, 0 },
+    movSpeed = { 0.075, 0.075, 0.075 },
+    rotSpeed = { 0.75, 0.75, 0.75 }
+}
+
+return ThirdPersonCamera;
