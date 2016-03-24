678e1636a1c809c2638c754b1b2125cfcdab5fc6
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-24 15:08:49 -0400

    Removed vDelta, vMovSpeed and vRotSpeed from Input component.

--- Source/Components/Input.hpp
+++ Source/Components/Input.hpp
@@ -1,14 +1,14 @@
 /*-------------------------------------------------------
                     <copyright>
-    
+
     File: Input.hpp
     Language: C++
-    
+
     (C) Copyright Eden Softworks
-    
+
     Author: Joshua Allen
     E-Mail: Joshua(AT)EdenSoftworks(DOT)net
-    
+
     Description: Header file for Input component.
 
 ---------------------------------------------------------*/
@@ -29,25 +29,20 @@
 namespace Components
 {
 	/** An Input component.
-	 *  The Input component provides access to a uint64_t bitmask which 
+	 *  The Input component provides access to a uint64_t bitmask which
 	 *  can be populated with values from the InputDictionary.
-	*/    
-    template <typename T>
-	struct Input : public Component
+	*/
+	typedef struct Input : public Component
 	{
 		CLASS_TYPE(Input);
 
 		uint64_t                    nMask;	/*!< A uint64_t bitmask. */
-        Leadwerks::Vec3             vDelta;
-        Leadwerks::Vec3             vMovSpeed;
-        Leadwerks::Vec3             vRotSpeed;        	
 
 		/** The Input component constructor.*/
-		explicit Input(Leadwerks::Vec3 _vMovSpeed = Leadwerks::Vec3(0.0f, 0.0f, 0.0f), Leadwerks::Vec3 _vRotSpeed = Leadwerks::Vec3(0.0f, 0.0f, 0.0f), std::string cName = "") 
-            : nMask(INPUT_NONE),  vDelta(0.0f, 0.0f, 0.0f), vMovSpeed(_vMovSpeed)
-            , vRotSpeed(_vRotSpeed), Component(cName) { }
+		explicit Input(std::string cName = "")
+            : nMask(INPUT_NONE), Component(cName) { }
 
-	}; // < end struct.
+	} Input; // < end struct.
 
 } // < end namespace.
 
