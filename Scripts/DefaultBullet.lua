38ab6f70b3f42f712db60ad4bb241a75df761945
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-24 11:21:27 -0400

    Created default bullet script file.

---
+++ Scripts/DefaultBullet.lua
@@ -0,0 +1,15 @@
+-- defaultBullet.lua
+
+Bullet = 
+{
+    name = "Bullet",    
+    modelPath = "box",    
+    pos = { 0, 0, 0 },
+    rot = { 0, 0, 0 },
+    sca = { 0.25, 0.25, 0.25 },
+    movSpeed = { 1.0, 1.0, 1.0 },
+    rotSpeed = { 0.75, 0.75, 0.75 },
+    damage = 2.5
+}
+
+return Bullet;
