cad84cc6e49372dd3a90516039ed92b4ebe1a2c2
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-24 11:25:36 -0400

    Relationship component now only stores the index of the related entity.

--- Source/Components/Relationship.hpp
+++ Source/Components/Relationship.hpp
@@ -1,40 +1,40 @@
 /*-------------------------------------------------------
                     <copyright>
-    
+
     File: Relationship.hpp
     Language: C++
-    
+
     (C) Copyright Eden Softworks
-    
+
     Author: Joshua Allen
     E-Mail: Joshua(AT)EdenSoftworks(DOT)net
-    
+
     Description: Header file for Relationship component.
 
 ---------------------------------------------------------*/
 
 #ifndef _RELATIONSHIP_COMPONENT_HPP_
     #define _RELATIONSHIP_COMPONENT_HPP_
-    
+
 #pragma once
+#include "../Common.hpp"
 #include "../Utilities/Macros.hpp"
 
 #include "Component.hpp"
 
 namespace Components
 {
-    template <typename T>
     typedef struct Relationship : public Component
     {
         CLASS_TYPE(Relationship);
-        
-        T               related;
-        
-        Relationship(T related, std::string cName = "")
-            : Component(cName) { }
-        
+
+        uint64_t               related;
+
+        Relationship(uint64_t _related = 0, std::string cName = "")
+            : related(related), Component(cName) { }
+
     }; // < end struct.
-    
+
 } // < end namespace.
 
 #endif _RELATIONSHIP_COMPONENT_HPP_
