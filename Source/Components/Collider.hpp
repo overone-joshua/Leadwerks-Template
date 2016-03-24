c90def93188f8d1a89aa4692b7c74fe83f1d7e10
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-24 11:24:17 -0400

    Collider component now uses AABB.

--- Source/Components/Collider.hpp
+++ Source/Components/Collider.hpp
@@ -1,22 +1,23 @@
 /*-------------------------------------------------------
                     <copyright>
-    
+
     File: Collider.hpp
     Language: C++
-    
+
     (C) Copyright Eden Softworks
-    
+
     Author: Joshua Allen
     E-Mail: Joshua(AT)EdenSoftworks(DOT)net
-    
+
     Description: Header file for Collider component.
 
 ---------------------------------------------------------*/
 
 #ifndef _COLLIDER_COMPONENT_HPP_
     #define _COLLIDER_COMPONENT_HPP_
-    
+
 #pragma once
+#include "Leadwerks.h"
 #include "../Utilities/Macros.hpp"
 #include "Component.hpp"
 
@@ -24,19 +25,18 @@
 
 namespace Components
 {
-    template <typename T>
     typedef struct Collider : public Component
     {
         CLASS_TYPE(Collider);
-        
-        T           collider;       /* The collider of type T.*/
-        
+
+        Leadwerks::AABB           collider;       /* The collider of type T.*/
+
         /** The Collider component constructor. */
-        Collider(T _collider, std::string cName = "")
+        Collider(Leadwerks::AABB _collider = Leadwerks::AABB(Leadwerks::Vec3(0.0f, 0.0f, 0.0f), Leadwerks::Vec3(0.0f, 0.0f, 0.0f)), std::string cName = "")
             : collider(_collider), Component(cName) { }
-            
+
     }; // < end struct.
-    
+
 } // < end namespace.
 
 #endif _COLLIDER_COMPONENT_HPP_
