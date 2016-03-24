b5d4c7674d728be71c145abd84287d3acd8a8651
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-24 15:10:08 -0400

    Added set operator to Component base.

--- Source/Components/Component.hpp
+++ Source/Components/Component.hpp
@@ -1,21 +1,21 @@
 /*-------------------------------------------------------
                     <copyright>
-    
+
     File: Component.hpp
     Language: C++
-    
+
     (C) Copyright Eden Softworks
-    
+
     Author: Joshua Allen
     E-Mail: Joshua(AT)EdenSoftworks(DOT)net
-    
+
     Description: Header file for base Component class.
 
 ---------------------------------------------------------*/
 
 #ifndef _COMPONENT_HPP_
 	#define _COMPONENT_HPP_
-	
+
 #pragma once
 #include "HasId.hpp"
 #include "HasName.hpp"
@@ -29,11 +29,14 @@ namespace Components
 	*/
 	typedef struct Component : public HasId, public HasName
 	{
-		using HasId::HasId;
-		using HasName::HasName;
-
 		/** The Components constructor. */
 		explicit Component(std::string cName = "") : HasName(cName) { }
+
+        Component& operator = (Component other)
+        {
+            this->nId = other.nId;
+            this->cName = other.cName;
+        }
 
 	} Component; // < end struct.
 
