aba246b850142a9e836176e3ad05704c24f08997
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-03-30 13:30:40 -0400

    Updated DbCommand to take a sqlite3 database as dependency.
    Also added basic query method in order to explicitely define sql-syntax queries.

--- Source/Utilities/sqlite/DbCommand.hpp
+++ Source/Utilities/sqlite/DbCommand.hpp
@@ -3,9 +3,17 @@
 
 #pragma once
 
+#include <sqlite/sqlite3.h>
+
+#include <cassert>
+#include <string>
+#include <vector>
+
 class IDbCommand
 {
 public:
+
+    virtual std::vector<std::vector<std::string>> Query(char* queryString) = 0
 
 }; // < end class interface.
 
@@ -13,9 +21,64 @@ class DbCommand : public IDbCommand
 {
 public:
 
-    void Dispose(void);
+    DbCommand(sqlite3* pDatabase)
+        : m_pDatabase(pDatabase)
+    {
+        assert(m_pDatabase != nullptr);
+    }
+
+    ~DbCommand(void)
+    {
+        Dispose();
+    }
+
+    std::vector<std::vector<std::string>> Query(char* queryString)
+    {
+        assert(m_pDatabase != nullptr);
+
+        sqlite3_stmt* pStatement;
+        vector<vector<string>> results;
+
+        auto res = sqlite3_prepare_v2(m_pDatabase, queryString, -1, &pStatement, 0);
+        assert(res == SQLITE_OK);
+
+        auto cols = sqlite3_column_count(pStatement);
+        auto result = 0;
+        while (true)
+        {
+            result = sqlite3_step(pStatement);
+
+            if (result == SQLITE_ROW)
+            {
+                std::vector<std::string> values;
+                auto index = cols;
+                while (index < cols)
+                {
+                    values.push_back((char*)sqlite3_column_text(pStatement, index));
+                    ++index;
+                }
+                results.push_back(values);
+            }
+            else
+            {
+                break;
+            }
+        }
+
+        auto err = sqlite3_errmsg(m_pDatabase);
+        if (err != "not an error") std::cerr << queryString << " " << err << std::endl;
+
+        return results;
+    }
+
+    void Dispose(void)
+    {
+
+    }
 
 protected:
+
+    sqlite3* const m_pDatabase;
 
 }; // < end class.
 
