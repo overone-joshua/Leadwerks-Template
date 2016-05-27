9ffe2e2f37410f642d7fbd3df87e14765cdb2971
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-05-27 08:31:58 -0400

    Removed old component implementations. Will be rewritten in conjunction with their required data tables and sqlite CRUD persistence methods.

--- Source/Components/ComponentDictionary.hpp
+++ Source/Components/ComponentDictionary.hpp
@@ -1,14 +1,14 @@
 /*-------------------------------------------------------
                     <copyright>
-    
+
     File: ComponentDictionary.hpp
     Language: C++
-    
+
     (C) Copyright Eden Softworks
-    
+
     Author: Joshua Allen
     E-Mail: Joshua(AT)EdenSoftworks(DOT)net
-    
+
     Description: Header file for ComponentDictionary.
 
 ---------------------------------------------------------*/
@@ -27,18 +27,12 @@ typedef enum
 {
 	COMPONENT_NONE = 0,
 	COMPONENT_PLACEMENT = 1 << 0,
-	COMPONENT_VELOCITY = 1 << 1,
-	COMPONENT_APPEARANCE = 1 << 2,
-	COMPONENT_CAMERA = 1 << 3,
-	COMPONENT_HASID = 1 << 4,
-	COMPONENT_HASNAME = 1 << 5,
-	COMPONENT_WORLD = 1 << 6,
-	COMPONENT_INPUT = 1 << 7,
-    COMPONENT_HEALTH = 1 << 8,
-    COMPONENT_TRIGGER = 1 << 9,
-    COMPONENT_AI = 1 << 10,
-    COMPONENT_RELATIONSHIP = 1 << 11,
-    COMPONENT_COLLIDER = 1 << 12    
+	COMPONENT_APPEARANCE = 1 << 1,
+	COMPONENT_CAMERA = 1 << 2,
+	COMPONENT_HASID = 1 << 3,
+	COMPONENT_HASNAME = 1 << 4,
+	COMPONENT_WORLD = 1 << 5,
+	COMPONENT_INPUT = 1 << 6,
 
 } ComponentDictionary;
 
