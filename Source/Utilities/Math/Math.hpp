91515d6f6ae002bd7883abf739a248bcb8130a03
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-05-31 07:22:19 -0400

    Added helper methods in order to generate a 4x4 matrix being rotated around a singular axis.

---
+++ Source/Utilities/Math/Math.hpp
@@ -0,0 +1,48 @@
+#ifndef _MATH_HPP_
+	#define _MATH_HPP_
+
+#pragma once
+#include "Leadwerks.h"
+
+namespace Math
+{
+
+	static Leadwerks::Mat4* MatrixRotationX(Leadwerks::Mat4* pOut, float angle)
+	{
+		pOut->MakeIdentity();
+
+		pOut[1][1] = Leadwerks::Math::Cos(angle);
+		pOut[2][2] = Leadwerks::Math::Cos(angle);
+		pOut[1][2] = Leadwerks::Math::Sin(angle);
+		pOut[2][1] = -Leadwerks::Math::Sin(angle);
+
+		return pOut;
+	}
+
+	static Leadwerks::Mat4* MatrixRotationY(Leadwerks::Mat4* pOut, float angle)
+	{
+		pOut->MakeIdentity();
+
+		pOut[0][0] = Leadwerks::Math::Cos(angle);
+		pOut[2][2] = Leadwerks::Math::Cos(angle);
+		pOut[0][2] = -Leadwerks::Math::Sin(angle);
+		pOut[2][0] = Leadwerks::Math::Sin(angle);
+
+		return pOut;
+	}
+
+	static Leadwerks::Mat4* MatrixRotationZ(Leadwerks::Mat4* pOut, float angle)
+	{
+		pOut->MakeIdentity();
+
+		pOut[0][0] = Leadwerks::Math::Cos(angle);
+		pOut[1][1] = Leadwerks::Math::Cos(angle);
+		pOut[0][1] = Leadwerks::Math::Sin(angle);
+		pOut[1][0] = -Leadwerks::Math::Sin(angle);
+
+		return pOut;
+	}
+
+} // < end namespace.
+
+#endif _MATH_HPP_
