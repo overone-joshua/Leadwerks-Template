7d2e49f41e983c135bcf203308c8a573c1207e70
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-06-27 12:54:05 -0400

    Updated TypeConverter to contain ConvertAll method.
    ConvertAll will allow users to easily pass in a collection of domain models and convert them to their persistence model equivilent through template specialization.

--- Source/Mapping/TypeConverter.hpp
+++ Source/Mapping/TypeConverter.hpp
@@ -1,31 +1,35 @@
 /*-------------------------------------------------------
                     <copyright>
-    
+
     File: Converter.hpp
     Language: C++
-    
+
     (C) Copyright Eden Softworks
-    
+
     Author: Joshua Allen
     E-Mail: Joshua(AT)EdenSoftworks(DOT)net
-    
+
     Description: Header file for type Converter.
 
-    Functions: 
+    Functions:
 ---------------------------------------------------------*/
 
 #ifndef _TYPE_CONVERTER_HPP_
     #define _TYPE_CONVERTER_HPP_
 
 #pragma once
+#include <vector>
 
 struct TypeConverter
 {
     TypeConverter() { }
 
     template <typename Inbound, typename Outbound>
-    static Outbound Convert(Inbound& source);
-        
+    static Outbound Convert(const Inbound& source);
+
+    template <typename Inbound, typename Outbound>
+    static std::vector<Outbound> ConvertAll(const std::vector<Inbound>& source);
+
 }; // end class.
 
 #endif _TYPE_CONVERTER_HPP_
