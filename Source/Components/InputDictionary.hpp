96de3aa8b4281fe22a0922f33bcfa32efeb0363f
Joshua J Allen <joshua(dot)allen(at)egov(dot)com>
2016-07-25 08:22:23 -0400

    Added Editor input enumerations to Input dictionary.

--- Source/Components/InputDictionary.hpp
+++ Source/Components/InputDictionary.hpp
@@ -17,12 +17,13 @@
 	#define _INPUT_DICTIONARY_HPP_
 
 #pragma once
+#include "../Common.hpp"
 
 /** An Input dictionary.
 *  An enumeration containing all bitmasks representing the finite
 *  number of inputs that can be combined together.
 */
-typedef enum
+typedef enum InputDictionary : unsigned __int64
 {
 	INPUT_NONE		= 0,
 	SYSTEM_INPUT	= 1,
@@ -45,7 +46,23 @@ typedef enum
 	INPUT_ACTION_JUMP		= USER_INPUT << 14,
 	INPUT_ACTION_INTERACT	= USER_INPUT << 15,
 	INPUT_ACTION_PRIMARY	= USER_INPUT << 16,
-	INPUT_ACTION_SECONDARY	= USER_INPUT << 17
+	INPUT_ACTION_SECONDARY	= USER_INPUT << 17,
+
+    EDITOR_INPUT                    = USER_INPUT << 18,
+    EDITOR_ACTION_ADD_VOXEL         = EDITOR_INPUT << 19,
+    EDITOR_ACTION_REMOVE_VOXEL      = EDITOR_INPUT << 20,
+    EDITOR_ACTION_SET_CURRENT_VOXEL = EDITOR_INPUT << 21,
+    EDITOR_ACTION_SAVE_LEVEL        = EDITOR_INPUT << 22,
+    EDITOR_ACTION_SAVE_LEVEL_AS     = EDITOR_INPUT << 23,
+    EDITOR_ACTION_OPEN_LEVEL        = EDITOR_INPUT << 24,
+    EDITOR_ACTION_ZOOM_IN           = EDITOR_INPUT << 25,
+    EDITOR_ACTION_ZOOM_OUT          = EDITOR_INPUT << 26,
+    EDITOR_ACTION_UNDO              = EDITOR_INPUT << 27,
+    EDITOR_ACTION_REDO              = EDITOR_INPUT << 28,
+    EDITOR_ACTION_SET_VOXEL         = EDITOR_INPUT << 29,
+    EDITOR_ACTION_SELECT_MATERIAL   = EDITOR_INPUT << 30,
+    EDITOR_ACTION_SELECT_SHADER     = EDITOR_INPUT << 31,
+    EDITOR_ACTION_CLEAR_LEVEL       = EDITOR_INPUT << 32
 
 } InputDictionary;
 
